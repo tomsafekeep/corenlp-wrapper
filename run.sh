b3359f0d14e7e33b60a39ae111f59765445a3b9d
Tom Korach <tom@safekeep.com>
2022-04-12 13:54:58 +0000

    Make classpath read from the user's home directory

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-java -cp $(cat cp.txt):target/corenlp-wrapper-1.0-SNAPSHOT.jar com.safekeep.nlp.corenlp.DatabaseTextProcessor "$1"
+java -cp $(sed "s/~/"${HOME//\//\\\/}"/g" cp.txt):target/corenlp-wrapper-1.0-SNAPSHOT.jar com.safekeep.nlp.corenlp.DatabaseTextProcessor "$1"
